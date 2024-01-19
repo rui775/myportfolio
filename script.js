41bd14a4e785148649595df113faec5f6dc8db14
rui775 <rsilva6420@gmail.com>
2024-01-19 12:20:02 +0000

    Responsiveness improved.

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const darkModeToggle = document.getElementById("dark-mode-toggle");
+    const darkModeToggleSm = document.getElementById("dark-mode-toggle");
+    const darkModeToggle = document.getElementById("dark-mode-toggle1");
     const body = document.body;
+
+    darkModeToggleSm.addEventListener("click", function () {
+        // Toggle dark mode by adding/removing a 'dark-mode' class to the body
+        body.classList.toggle("dark-mode");
+
+        // Save user preference in local storage
+        const isDarkMode = body.classList.contains("dark-mode");
+        localStorage.setItem("darkMode", isDarkMode);
+    });
 
     darkModeToggle.addEventListener("click", function () {
         // Toggle dark mode by adding/removing a 'dark-mode' class to the body
